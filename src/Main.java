e2a1467bc12abfc0e22c7da58e9b0a314cec619b
JakariaShrabon <jakariabhuiyanshrabon@gmail.com>
2023-09-29 22:04:48 +0600

    Hash_Map

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String,Integer> employee_ID= new HashMap<>();
+        employee_ID.put("Jakaria",01122);
+        employee_ID.put("Juli",01123);
+        System.out.println(employee_ID);
+    }
+}
